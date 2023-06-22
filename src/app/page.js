adabb0247f1a51294666ab77bd42c8a32add8d26
Danila <danea48@gmail.com>
2023-06-22 13:37:41 +0300

    penultimate commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,18 +1,13 @@
+'use client'
 import axios from 'axios';
 import Header from '../components/Header';
 import Users from '../components/Users';
+
 export default function Home() {
-
-  // async function fetchPosts(){
-  //   const response = await axios.get('https://jsonplaceholder.typicode.com/users')
-  //   console.log(response.data);
-  // }
-
+  // useClient();
   return (
-    
     <main>
       <Header />
-      {/* <button onClick={fetchPosts()} className=''>Take data</button> */}
       <Users/>
     </main>
 
--- src/components/Users.js
+++ src/components/Users.js
@@ -1,207 +1,55 @@
+
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import Image from "next/image"
+
 export default function Users() {
+
+    const [users, setUsers] = useState([])
+
+    useEffect(() => {
+        axios.get('https://jsonplaceholder.typicode.com/users')
+            .then((response) => response.data)
+            .then((data) => setUsers(data))
+            .catch((error) => console.log(error));
+    }, [])
+
+
     return (
-        <div className="grid grid-rows-5 grid-flow-col gap-4 w-[85rem] mb-8">
-            {/* Блок 1 */}
-            <div className="flex bg-white w-[41rem] h-[12rem] mt-8 ml-8 justify-between " id="Profile1">
-                <Image src="/img1.jpg" width="120" height="120" alt="webDev" loading="lazy" className="m-[1.875rem]" />
-                <div className="grid bg-transparent mt-[1.25rem] w-[10rem] h-[10rem]">
-                    <span className="bg-transparent text-red-600 font-semibold"> Name</span>
-                    <span className="bg-transparent"> Polia</span>
-                    <span className="bg-transparent text-red-600 font-semibold"> Phone</span>
-                    <span className="bg-transparent"> fdfdfddf</span>
-                    <span className="bg-transparent text-red-600 font-semibold"> Website</span>
-                    <span className="bg-transparent">dfdfdfdfdf</span>
-                </div>
-                <div className="grid bg-transparent mt-[1.25rem] w-[20rem] h-[6.875rem]">
-                    <span className="bg-transparent text-red-600 font-semibold">Email </span>
-                    <span className="bg-transparent"> Polia</span>
-                    <span className="bg-transparent text-red-600 font-semibold">Address</span>
-                    <span className="bg-transparent"> Polia</span>
-
-                </div>
+        <main>
+            <div className="grid grid-rows-5 grid-flow-col gap-4 w-[85rem] mb-8">
+                {users.map((users, index) => (
+                    <div
+                        key={users.id}
+                        className={`flex bg-white w-[41rem] h-[12rem] mt-8 ml-8 justify-between`}
+                        id={`Profile${index + 1}`}>
+                        <Image
+                            src={`/img${index + 1}.jpg`}
+                            alt={`Person${index + 1}`}
+                            className="m-[1.875rem] w-auto h-auto"
+                            width="120"
+                            height="120"
+                            loading="lazy"
+                        />
+                        <div className="grid bg-transparent mt-[1.25rem] w-[10rem] h-[10rem]">
+                            <span className="bg-transparent text-red-600 font-semibold"> Name</span>
+                            <span className="bg-transparent">{users.name}</span>
+                            <span className="bg-transparent text-red-600 font-semibold"> Phone</span>
+                            <span className="bg-transparent">{users.phone}</span>
+                            <span className="bg-transparent text-red-600 font-semibold"> Website</span>
+                            <span className="bg-transparent">{users.website}</span>
+                        </div>
+                        <div className="grid bg-transparent mt-[1.25rem] w-[20rem] h-[6.875rem]">
+                            <span className="bg-transparent text-red-600 font-semibold">Email </span>
+                            <span className="bg-transparent">{users.email}</span>
+                            <span className="bg-transparent text-red-600 font-semibold">Address</span>
+                            <span className="bg-transparent">{users.address?.street}, {users.address.suite}, {users.address.city}, {users.address.zipcode}</span>
+                        </div>
+                    </div>
+                ))}
+                
             </div>
+        </main>
 
-            {/* Блок 2 */}
-            <div className="flex bg-white w-[41rem] h-[12rem] mt-8 ml-8 justify-between " id="Profile2">
-                <Image src="/img2.jpg" width="120" height="120" alt="webDev" loading="lazy" className="m-[1.875rem]" />
-                <div className="grid bg-transparent mt-[1.25rem] w-[10rem] h-[10rem]">
-                    <span className="bg-transparent text-red-600 font-semibold"> Name</span>
-                    <span className="bg-transparent"> Polia</span>
-                    <span className="bg-transparent text-red-600 font-semibold"> Phone</span>
-                    <span className="bg-transparent"> fdfdfddf</span>
-                    <span className="bg-transparent text-red-600 font-semibold"> Website</span>
-                    <span className="bg-transparent">dfdfdfdfdf</span>
-                </div>
-                <div className="grid bg-transparent mt-[1.25rem] w-[20rem] h-[6.875rem]">
-                    <span className="bg-transparent text-red-600 font-semibold">Email </span>
-                    <span className="bg-transparent"> Polia</span>
-                    <span className="bg-transparent text-red-600 font-semibold">Address</span>
-                    <span className="bg-transparent"> Polia</span>
-
-                </div>
-            </div>
-
-            {/* Блок 3 */}
-            <div className="flex bg-white w-[41rem] h-[12rem] mt-8 ml-8 justify-between " id="Profile3">
-                <Image src="/img3.jpg" width="120" height="120" alt="webDev" loading="lazy" className="m-[1.875rem]" />
-                <div className="grid bg-transparent mt-[1.25rem] w-[10rem] h-[10rem]">
-                    <span className="bg-transparent text-red-600 font-semibold"> Name</span>
-                    <span className="bg-transparent"> Polia</span>
-                    <span className="bg-transparent text-red-600 font-semibold"> Phone</span>
-                    <span className="bg-transparent"> fdfdfddf</span>
-                    <span className="bg-transparent text-red-600 font-semibold"> Website</span>
-                    <span className="bg-transparent">dfdfdfdfdf</span>
-                </div>
-                <div className="grid bg-transparent mt-[1.25rem] w-[20rem] h-[6.875rem]">
-                    <span className="bg-transparent text-red-600 font-semibold">Email </span>
-                    <span className="bg-transparent"> Polia</span>
-                    <span className="bg-transparent text-red-600 font-semibold">Address</span>
-                    <span className="bg-transparent"> Polia</span>
-
-                </div>
-            </div>
-
-            {/* Блок 4 */}
-            <div className="flex bg-white w-[41rem] h-[12rem] mt-8 ml-8 justify-between " id="Profile4">
-                <Image src="/img4.jpg" width="120" height="120" alt="webDev" loading="lazy" className="m-[1.875rem]" />
-                <div className="grid bg-transparent mt-[1.25rem] w-[10rem] h-[10rem]">
-                    <span className="bg-transparent text-red-600 font-semibold"> Name</span>
-                    <span className="bg-transparent"> Polia</span>
-                    <span className="bg-transparent text-red-600 font-semibold"> Phone</span>
-                    <span className="bg-transparent"> fdfdfddf</span>
-                    <span className="bg-transparent text-red-600 font-semibold"> Website</span>
-                    <span className="bg-transparent">dfdfdfdfdf</span>
-                </div>
-                <div className="grid bg-transparent mt-[1.25rem] w-[20rem] h-[6.875rem]">
-                    <span className="bg-transparent text-red-600 font-semibold">Email </span>
-                    <span className="bg-transparent"> Polia</span>
-                    <span className="bg-transparent text-red-600 font-semibold">Address</span>
-                    <span className="bg-transparent"> Polia</span>
-
-                </div>
-            </div>
-
-            {/* Блок 5 */}
-            <div className="flex bg-white w-[41rem] h-[12rem] mt-8 ml-8 justify-between " id="Profile5">
-                <Image src="/img5.jpg" width="120" height="120" alt="webDev" className="m-[1.875rem]" />
-                <div className="grid bg-transparent mt-[1.25rem] w-[10rem] h-[10rem]">
-                    <span className="bg-transparent text-red-600 font-semibold"> Name</span>
-                    <span className="bg-transparent"> Polia</span>
-                    <span className="bg-transparent text-red-600 font-semibold"> Phone</span>
-                    <span className="bg-transparent"> fdfdfddf</span>
-                    <span className="bg-transparent text-red-600 font-semibold"> Website</span>
-                    <span className="bg-transparent">dfdfdfdfdf</span>
-                </div>
-                <div className="grid bg-transparent mt-[1.25rem] w-[20rem] h-[6.875rem]">
-                    <span className="bg-transparent text-red-600 font-semibold">Email </span>
-                    <span className="bg-transparent"> Polia</span>
-                    <span className="bg-transparent text-red-600 font-semibold">Address</span>
-                    <span className="bg-transparent"> Polia</span>
-
-                </div>
-            </div>
-
-            {/* Блок 6 */}
-            <div className="flex bg-white w-[41rem] h-[12rem] mt-8 ml-8 justify-between " id="Profile6">
-                <Image src="/img6.jpg" width="120" height="120" alt="webDev" loading="lazy" className="m-[1.875rem]" />
-                <div className="grid bg-transparent mt-[1.25rem] w-[10rem] h-[10rem]">
-                    <span className="bg-transparent text-red-600 font-semibold"> Name</span>
-                    <span className="bg-transparent"> Polia</span>
-                    <span className="bg-transparent text-red-600 font-semibold"> Phone</span>
-                    <span className="bg-transparent"> fdfdfddf</span>
-                    <span className="bg-transparent text-red-600 font-semibold"> Website</span>
-                    <span className="bg-transparent">dfdfdfdfdf</span>
-                </div>
-                <div className="grid bg-transparent mt-[1.25rem] w-[20rem] h-[6.875rem]">
-                    <span className="bg-transparent text-red-600 font-semibold">Email </span>
-                    <span className="bg-transparent"> Polia</span>
-                    <span className="bg-transparent text-red-600 font-semibold">Address</span>
-                    <span className="bg-transparent"> Polia</span>
-
-                </div>
-            </div>
-
-            {/* Блок 7 */}
-            <div className="flex bg-white w-[41rem] h-[12rem] mt-8 ml-8 justify-between " id="Profile7">
-                <Image src="/img7.jpg" width="120" height="120" alt="webDev" className="m-[1.875rem]" />
-                <div className="grid bg-transparent mt-[1.25rem] w-[10rem] h-[10rem]">
-                    <span className="bg-transparent text-red-600 font-semibold"> Name</span>
-                    <span className="bg-transparent"> Polia</span>
-                    <span className="bg-transparent text-red-600 font-semibold"> Phone</span>
-                    <span className="bg-transparent"> fdfdfddf</span>
-                    <span className="bg-transparent text-red-600 font-semibold"> Website</span>
-                    <span className="bg-transparent">dfdfdfdfdf</span>
-                </div>
-                <div className="grid bg-transparent mt-[1.25rem] w-[20rem] h-[6.875rem]">
-                    <span className="bg-transparent text-red-600 font-semibold">Email </span>
-                    <span className="bg-transparent"> Polia</span>
-                    <span className="bg-transparent text-red-600 font-semibold"> Address</span>
-                    <span className="bg-transparent"> Polia</span>
-
-                </div>
-            </div>
-
-            {/* Блок 8 */}
-            <div className="flex bg-white w-[41rem] h-[12rem] mt-8 ml-8 justify-between " id="Profile8">
-                <Image src="/img8.jpg" width="120" height="120" alt="webDev" className="m-[1.875rem]" />
-                <div className="grid bg-transparent mt-[1.25rem] w-[10rem] h-[10rem]">
-                    <span className="bg-transparent text-red-600 font-semibold"> Name</span>
-                    <span className="bg-transparent"> Polia</span>
-                    <span className="bg-transparent text-red-600 font-semibold"> Phone</span>
-                    <span className="bg-transparent"> fdfdfddf</span>
-                    <span className="bg-transparent text-red-600 font-semibold"> Website</span>
-                    <span className="bg-transparent">dfdfdfdfdf</span>
-                </div>
-                <div className="grid bg-transparent mt-[1.25rem] w-[20rem] h-[6.875rem]">
-                    <span className="bg-transparent text-red-600 font-semibold">Email </span>
-                    <span className="bg-transparent"> Polia</span>
-                    <span className="bg-transparent text-red-600 font-semibold"> Address</span>
-                    <span className="bg-transparent"> Polia</span>
-
-                </div>
-            </div>
-
-            {/* Блок 9 */}
-            <div className="flex bg-white w-[41rem] h-[12rem] mt-8 ml-8 justify-between " id="Profile9">
-                <Image src="/img9.jpg" width="120" height="120" alt="webDev" className="m-[1.875rem]" />
-                <div className="grid bg-transparent mt-[1.25rem] w-[10rem] h-[10rem]">
-                    <span className="bg-transparent text-red-600 font-semibold"> Name</span>
-                    <span className="bg-transparent"> Polia</span>
-                    <span className="bg-transparent text-red-600 font-semibold"> Phone</span>
-                    <span className="bg-transparent"> fdfdfddf</span>
-                    <span className="bg-transparent text-red-600 font-semibold"> Website</span>
-                    <span className="bg-transparent">dfdfdfdfdf</span>
-                </div>
-                <div className="grid bg-transparent mt-[1.25rem] w-[20rem] h-[6.875rem]">
-                    <span className="bg-transparent text-red-600 font-semibold">Email </span>
-                    <span className="bg-transparent"> Polia</span>
-                    <span className="bg-transparent text-red-600 font-semibold"> Address</span>
-                    <span className="bg-transparent"> Polia</span>
-
-                </div>
-            </div>
-
-            {/* Блок 10 */}
-            <div className="flex bg-white w-[41rem] h-[12rem] mt-8 ml-8 justify-between " id="Profile10">
-                <Image src="/img10.jpg" width="120" height="120" alt="webDev" className="m-[1.875rem]" />
-                <div className="grid bg-transparent mt-[1.25rem] w-[10rem] h-[10rem]">
-                    <span className="bg-transparent text-red-600 font-semibold"> Name</span>
-                    <span className="bg-transparent"> Polia</span>
-                    <span className="bg-transparent text-red-600 font-semibold"> Phone</span>
-                    <span className="bg-transparent"> fdfdfddf</span>
-                    <span className="bg-transparent text-red-600 font-semibold"> Website</span>
-                    <span className="bg-transparent">dfdfdfdfdf</span>
-                </div>
-                <div className="grid bg-transparent mt-[1.25rem] w-[20rem] h-[6.875rem]">
-                    <span className="bg-transparent text-red-600 font-semibold">Email </span>
-                    <span className="bg-transparent"> Polia</span>
-                    <span className="bg-transparent text-red-600 font-semibold"> Address</span>
-                    <span className="bg-transparent"> Polia</span>
-
-                </div>
-            </div>
-
-        </div>
     )
 }
